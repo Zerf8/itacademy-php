9e6b2299e293f887da3c47ecea2efad316c2baa6
zerf <zerfnba@gmail.com>
2022-03-04 17:38:33 +0100

    inicio Ejercicio 8 nivell 3 y carpeta

---
+++ Tema3_Nivell3/exercici8.php
@@ -0,0 +1,12 @@
+<?php
+    // - Exercici 1
+    // Escriu un programa en PHP per convertir una cadena en un array 
+    //(retallant cada caràcter i eliminant les línies buides).
+    
+    // Entrada
+    // Hello world
+    
+    // Sortida
+    // array(10) { [0]=> string(1) "H" [1]=> string(1) "e" [2]=> string(1) "l" [3]=> string(1) "l" [4]=> string(1) "o" 
+    //    [5]=> string(1) "W" [6]=> string(1) "o" [7]=> string(1) "r" [8]=> string(1) "l" [9]=> string(1) "d"}
+?>
